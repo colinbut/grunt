3d9ad32c2e412d5bf836801345ce3c57ef9d510f
Colin But <colinbut@users.noreply.github.com>
2017-01-14 22:18:51 +0000

    added the gruntfile with some tasks registered

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+// wrapper function
+module.exports = function(grunt) {
+
+  // project configuration - define tasks here
+  // Grunt tasks (most of them) rely on configuration data (configuration object)
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // loading the plugin that does that uglify job
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // set default Task
+  grunt.registerTask('default', ['uglify']);
+
+};
